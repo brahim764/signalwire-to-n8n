942d08a5b59f5dcae7b7df14513f7ae77f6ae505
brahim764 <brahimsabri406@gmail.com>
2025-06-20 13:56:02 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const port = process.env.PORT || 10000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 app.post('/call', (req, res) => {
   res.set('Content-Type', 'text/xml');
-  res.send(`
-    <?xml version="1.0" encoding="UTF-8"?>
-    <Response>
-      <Say language="fr-FR">Test direct Render, si vous entendez ce message, la connexion SignalWire vers Render fonctionne.</Say>
-    </Response>
-  `);
+  res.send('<?xml version="1.0" encoding="UTF-8"?><Response><Say language="fr-FR">Test direct Render, la connexion SignalWire vers Render fonctionne.</Say></Response>');
 });
 
-const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => {
-  console.log('Server started on port', PORT);
+app.listen(port, () => {
+  console.log('Server started on port ' + port);
 });
